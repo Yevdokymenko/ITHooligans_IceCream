90939e73d78ad38aa35e18a99436dbebd2023ce4
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-06-28 17:26:17 +0000

    Deploying to gh-pages from @ Yevdokymenko/ITHooligans_IceCream@57025cc230b6277100457a058ff5712f81821b1b 🚀

---
+++ index.560ac489.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-2]"),closeModalBtn:document.querySelector("[data-modal-close-2]"),modal:document.querySelector("[data-modal-2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.560ac489.js.map
