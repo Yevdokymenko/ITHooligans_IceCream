465ae0db5412559ecb633cc16e22b9024cca89e2
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-06-29 19:41:17 +0300

    fix js

--- src/js/modal-1.js
+++ src/js/modal-1.js
@@ -7,6 +7,7 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
+    document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
