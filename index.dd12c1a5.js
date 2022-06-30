05e6c4f018503e127b783f2360c7adbab9e4a6d9
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-06-30 10:35:16 +0000

    Deploying to gh-pages from @ Yevdokymenko/ITHooligans_IceCream@370476e03163f7c0d4a5633b60b6f7c04c8d408c 🚀

---
+++ index.dd12c1a5.js
@@ -0,0 +1,2 @@
+var scroll_distance=100,transparent=!0;document.addEventListener("scroll",(function(){$(document).scrollTop()>scroll_distance?transparent&&(transparent=!1,$("header").addClass("changeColor")):transparent||(transparent=!0,$("header").removeClass("changeColor"))}));
+//# sourceMappingURL=index.dd12c1a5.js.map
